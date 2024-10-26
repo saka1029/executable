d0b826fa77bfbab990e9fa867042f664f13d6b1a
saka1029 <saka1029@gmail.com>
2024-10-26 21:41:24 +0900

    ..

--- src/main/java/saka1029/executable/Define.java
+++ src/main/java/saka1029/executable/Define.java
@@ -13,17 +13,7 @@ public class Define extends SymbolMacro {
     @Override
     public void execute(Context c) {
         Executable body = c.pop();  // defineしたときの定義内容を保存
-        c.globals.put(symbol, new Executable() {
-            @Override
-            public void execute(Context c) {
-                body.call(c);
-            }
-
-            @Override
-            public String toString() {
-                return body.toString();
-            }
-        });
+        c.globals.put(symbol, DefinedBody.of(body));
     }
 
     @Override
