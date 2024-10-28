bf8067ebb75de07316d01b94b5fe4d4b8e003977
saka1029 <saka1029@gmail.com>
2024-10-28 09:47:40 +0900

    ..

--- src/main/java/saka1029/executable/Parser.java
+++ src/main/java/saka1029/executable/Parser.java
@@ -4,17 +4,29 @@ import java.util.ArrayList;
 
 public class Parser {
 
-    String input;
+    int[] input;
     int index;
     StringBuilder text = new StringBuilder();
+    int ch;
 
     public List parse(String input) {
-        this.input = input;
+        this.input = input.codePoints().toArray();
         this.index = 0;
         this.text.setLength(0);
+        get();
         java.util.List<Executable> list = new ArrayList<>();
         return Cons.list(list);
     }
 
-}
+    int get() {
+        return ch = index < input.length ? input[index++] : -1;
+    }
 
+    Executable read() {
+        return switch (ch) {
+            case -1 -> null;
+            default -> throw new RuntimeException();
+        };
+    }
+
+}
