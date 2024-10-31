ab5213e55849859d66a3152e3ecf4b553758ef46
saka1029 <saka1029@gmail.com>
2024-10-31 23:23:34 +0900

    ..

--- src/main/java/saka1029/executable/Frame.java
+++ src/main/java/saka1029/executable/Frame.java
@@ -1,6 +1,6 @@
 package saka1029.executable;
 
-public class Frame {
+public class Frame implements Executable {
     
     final int arguments, returns;
     final List executables;
@@ -9,6 +9,20 @@ public class Frame {
         this.arguments = arguments;
         this.returns = returns;
         this.executables = executables;
+    }
+
+    @Override
+    public void execute(Context c) {
+        c.fstack.add(c.stack.size());
+        executables.execute(c);
+        // move return values in stack
+        int sp = c.stack.size(), fp = c.fstack.get(c.fstack.size() - 1), r = fp + returns;
+        for (int i = sp - returns, j = fp; i < returns; i++, j++)
+            c.stack.set(j, c.stack.get(i));
+        // drop stack
+        while (c.stack.size() > r)
+            c.pop();
+        c.fstack.remove(c.fstack.size() - 1);
     }
 
 }
--- src/main/java/saka1029/executable/ParseContext.java
+++ src/main/java/saka1029/executable/ParseContext.java
@@ -16,5 +16,6 @@ public class ParseContext {
     }
 
     Frame endFrame() {
+        return null;
     }
 }
